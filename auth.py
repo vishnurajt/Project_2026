06c6520eeabbff06e7ce673e075845c8171783a8
vishnurajt <vishnuraj.therayikkal@gmail.com>
2026-04-09 21:42:40 +0530

    CORS,APIRouter,Logging Middleware

--- auth.py
+++ auth.py
@@ -21,7 +21,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
-    
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
