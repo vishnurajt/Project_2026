8f469be6ef330a03f63be2eded26a27d251c8efa
vishnurajt <vishnuraj.therayikkal@gmail.com>
2026-04-12 15:47:38 +0530

    Fix database

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ database_url = settings.DATABASE_URL
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
